98abbf77038d029cd23b3a56f252f68beac7d6e6
Daniel <daniil.gavr@gmail.com>
2025-01-24 02:20:59 +0300

    Add logic for video media items and basic tests

--- pixventure_back/media/tests.py
+++ pixventure_back/media/tests.py
@@ -1,3 +1,199 @@
-from django.test import TestCase
+# test_blur_logic.py
 
-# Create your tests here.
+from django.test import TestCase
+from unittest.mock import patch
+from django.contrib.auth.models import User
+from posts.models import Post
+from media.models import MediaItem
+from taxonomy.models import Category
+from media.utils import get_media_file_for_display
+
+class BlurLogicTest(TestCase):
+    """
+    Tests the blur/paywall logic for both Photos and Videos.
+    Ensures the function returns the *full URL* (e.g. '/media/preview.jpg') 
+    or an empty string if no file is set.
+    """
+
+    def setUp(self):
+        # Create paying and non-paying users
+        self.user_paying = User.objects.create_user(username='paying_user')
+        self.user_non_paying = User.objects.create_user(username='non_paying_user')
+        
+        # Create a post category
+        self.category = Category.objects.create(name='Test category', slug='test-category-slug')
+
+        # Create a post (not blurred by default)
+        self.post = Post.objects.create(name='Test Post', is_blurred=False, main_category=self.category)
+
+        # PHOTO item with all file fields
+        # We'll assume Django's FileField populates .url with '/media/<filename>'
+        self.photo_item = MediaItem.objects.create(
+            item_type=MediaItem.PHOTO,
+            is_blurred=False,
+            watermarked_file='watermarked.jpg',          # => '/media/watermarked.jpg'
+            preview_file='preview.jpg',                  # => '/media/preview.jpg'
+            thumbnail_file='thumb.jpg',                  # => '/media/thumb.jpg'
+            blurred_preview_file='preview_blurred.jpg',  # => '/media/preview_blurred.jpg'
+            blurred_thumbnail_file='thumb_blurred.jpg',  # => '/media/thumb_blurred.jpg'
+        )
+
+        # VIDEO item with no blurred files
+        self.video_item = MediaItem.objects.create(
+            item_type=MediaItem.VIDEO,
+            is_blurred=False,
+            watermarked_file='watermarked_video.mp4',  # => '/media/watermarked_video.mp4'
+            preview_file='preview_video.mp4',          # => '/media/preview_video.mp4'
+            thumbnail_file='video_thumb.jpg',          # => '/media/video_thumb.jpg'
+            blurred_preview_file='',                   # no blurred version
+            blurred_thumbnail_file='',                 # no blurred version
+        )
+
+    # Helper to unify calling the function
+    def _get_url(self, media_item, user, post=None, thumbnail=False):
+        return get_media_file_for_display(media_item, user, post=post, thumbnail=thumbnail)
+
+    ### PHOTO TESTS ###
+
+    @patch('media.utils.check_if_user_is_paying', return_value=True)
+    def test_photo_nonblurred_paying_post_nonblurred_full(self, mock_check):
+        """
+        Photo item => paying user => not blurred => full => watermarked_file
+        Expect '/media/watermarked.jpg'
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.photo_item.is_blurred = False
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/watermarked.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=True)
+    def test_photo_nonblurred_paying_post_nonblurred_thumb(self, mock_check):
+        """
+        Photo item => paying user => not blurred => thumbnail => thumbnail_file
+        Expect '/media/thumb.jpg'
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.photo_item.is_blurred = False
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_paying, post=self.post, thumbnail=True)
+        self.assertEqual(url, '/media/thumb.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=False)
+    def test_photo_blurred_nonpaying_post_blurred_full(self, mock_check):
+        """
+        Photo item => non-paying => item/post blurred => full => blurred_preview_file
+        Expect '/media/preview_blurred.jpg'
+        """
+        self.post.is_blurred = True
+        self.post.save()
+        self.photo_item.is_blurred = True
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_non_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/preview_blurred.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=False)
+    def test_photo_blurred_nonpaying_post_blurred_thumb(self, mock_check):
+        """
+        Photo item => non-paying => item/post blurred => thumbnail => blurred_thumbnail_file
+        Expect '/media/thumb_blurred.jpg'
+        """
+        self.post.is_blurred = True
+        self.post.save()
+        self.photo_item.is_blurred = True
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_non_paying, post=self.post, thumbnail=True)
+        self.assertEqual(url, '/media/thumb_blurred.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=False)
+    def test_photo_nonblurred_nonpaying_post_nonblurred_full(self, mock_check):
+        """
+        Photo => non-paying => post/item not blurred => full => preview_file
+        Expect '/media/preview.jpg'
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.photo_item.is_blurred = False
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_non_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/preview.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=False)
+    def test_photo_nonblurred_nonpaying_post_nonblurred_thumb(self, mock_check):
+        """
+        Photo => non-paying => post/item not blurred => thumbnail => thumbnail_file
+        Expect '/media/thumb.jpg'
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.photo_item.is_blurred = False
+        self.photo_item.save()
+
+        url = self._get_url(self.photo_item, self.user_non_paying, post=self.post, thumbnail=True)
+        self.assertEqual(url, '/media/thumb.jpg')
+
+    ### Additional combos: post blurred, item not, or vice versa, can be added as needed ###
+
+
+    ### VIDEO TESTS (no blurred files) ###
+
+    @patch('media.utils.check_if_user_is_paying', return_value=True)
+    def test_video_nonblurred_paying_post_nonblurred_full(self, mock_check):
+        """
+        Video => paying => not blurred => full => watermarked_video.mp4
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.video_item.is_blurred = False
+        self.video_item.save()
+
+        url = self._get_url(self.video_item, self.user_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/watermarked_video.mp4')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=True)
+    def test_video_nonblurred_paying_post_nonblurred_thumb(self, mock_check):
+        """
+        Video => paying => not blurred => thumbnail => video_thumb.jpg
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.video_item.is_blurred = False
+        self.video_item.save()
+
+        url = self._get_url(self.video_item, self.user_paying, post=self.post, thumbnail=True)
+        self.assertEqual(url, '/media/video_thumb.jpg')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=True)
+    def test_video_blurred_paying_post_nonblurred_full(self, mock_check):
+        """
+        Video => paying => item is blurred => still we ignore blur for paying => watermarked_video.mp4
+        """
+        self.post.is_blurred = False
+        self.post.save()
+        self.video_item.is_blurred = True
+        self.video_item.save()
+
+        url = self._get_url(self.video_item, self.user_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/watermarked_video.mp4')
+
+    @patch('media.utils.check_if_user_is_paying', return_value=False)
+    def test_video_blurred_nonpaying_post_blurred_full(self, mock_check):
+        """
+        Video => non-paying => both post & item blurred => no blurred video file => fallback to preview_video.mp4
+        """
+        self.post.is_blurred = True
+        self.post.save()
+        self.video_item.is_blurred = True
+        self.video_item.save()
+
+        url = self._get_url(self.video_item, self.user_non_paying, post=self.post, thumbnail=False)
+        self.assertEqual(url, '/media/preview_video.mp4')
+
+    # You can add more combos if needed, e.g. post blurred but item not, user non-paying, etc.
--- pixventure_back/media/utils.py
+++ pixventure_back/media/utils.py
@@ -1,11 +1,28 @@
 from memberships.utils import check_if_user_is_paying
 
+from memberships.utils import check_if_user_is_paying
+from media.models import MediaItem
+
 def get_media_file_for_display(media_item, user, post=None, thumbnail=False):
     """
     Returns the appropriate file URL for a MediaItem given:
       - user's membership status
       - whether post or item is blurred
       - whether we want thumbnail or "full" file
+
+    Special logic for videos:
+      - Videos do not have blurred versions (blurred_thumbnail_file, blurred_preview_file).
+        If the video or post is blurred:
+           - paying users see watermarked_file
+           - non-paying users see preview_file
+           - thumbnail is always the normal thumbnail_file (if any)
+
+    Photos:
+      - If user is paying => always watermarked_file (or thumbnail_file if thumbnail=True)
+        ignoring blur
+      - If user is non-paying:
+          * If blurred => use blurred_preview_file or blurred_thumbnail_file
+          * If not blurred => use preview_file or thumbnail_file
     """
 
     # 1. Check if user is paying
@@ -15,29 +32,44 @@ def get_media_file_for_display(media_item, user, post=None, thumbnail=False):
     #    If the post is blurred OR the item is blurred => entire content is blurred
     post_blurred = getattr(post, 'is_blurred', False)  # post may be None if unknown
     item_blurred = media_item.is_blurred
-    is_blurred = post_blurred or item_blurred
+    is_blurred = (post_blurred or item_blurred)
 
-    # 3. Decide which field to use
-    if user_is_paying:
-        # Paying users see watermarked_file (full resolution) if not blurred
-        # But if the item/post is blurred, do we still want to serve blurred content 
-        # or do paying users see it unblurred? The business logic says:
-        # "A paying user always sees non-blurred content"
-        # so presumably we ignore the blur in that case:
-        if thumbnail:
-            return media_item.thumbnail_file.url if media_item.thumbnail_file else ""
-        else:
-            return media_item.watermarked_file.url if media_item.watermarked_file else ""
-    else:
-        # Non-paying user
-        # If blurred => return blurred version 
-        if is_blurred:
+    # 3. Separate logic if this is a PHOTO or VIDEO
+    if media_item.item_type == MediaItem.PHOTO:
+        # --- PHOTO LOGIC (supports blurred files) ---
+        if user_is_paying:
+            # Paying users see watermarked_file or thumbnail_file
             if thumbnail:
-                return media_item.blurred_thumbnail_file.url if media_item.blurred_thumbnail_file else ""
+                return media_item.thumbnail_file.url if media_item.thumbnail_file else ""
             else:
-                return media_item.blurred_preview_file.url if media_item.blurred_preview_file else ""
+                return media_item.watermarked_file.url if media_item.watermarked_file else ""
         else:
-            # Non-blurred => see normal preview 
+            # Non-paying user
+            if is_blurred:
+                # Return blurred version
+                if thumbnail:
+                    return media_item.blurred_thumbnail_file.url if media_item.blurred_thumbnail_file else ""
+                else:
+                    return media_item.blurred_preview_file.url if media_item.blurred_preview_file else ""
+            else:
+                # Return normal preview
+                if thumbnail:
+                    return media_item.thumbnail_file.url if media_item.thumbnail_file else ""
+                else:
+                    return media_item.preview_file.url if media_item.preview_file else ""
+    else:
+        # --- VIDEO LOGIC (no blurred files exist) ---
+        # If blurred => paying user sees watermarked, non-paying sees preview
+        if user_is_paying:
+            # Always see watermarked_file for paying users
+            if thumbnail:
+                # The thumbnail for a video is not blurred, so we use normal thumbnail_file if any
+                return media_item.thumbnail_file.url if media_item.thumbnail_file else ""
+            else:
+                return media_item.watermarked_file.url if media_item.watermarked_file else ""
+        else:
+            # Non-paying user
+            # Even if blurred, we do not have a "blurred" version, so we still serve preview_file
             if thumbnail:
                 return media_item.thumbnail_file.url if media_item.thumbnail_file else ""
             else:
