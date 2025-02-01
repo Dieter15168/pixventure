ef5bf65fa430de1cd1ba846d41bb9df584d93229
Daniel <daniil.gavr@gmail.com>
2025-02-01 21:08:53 +0300

    Add moderation admin classes

--- pixventure_back/moderation/admin.py
+++ pixventure_back/moderation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import RejectionReason, ModerationAction
 
-# Register your models here.
+@admin.register(RejectionReason)
+class RejectionReasonAdmin(admin.ModelAdmin):
+    list_display = ("order", "name", "is_active")
+    list_display_links = ("name",)  # Ensure 'order' is editable
+    list_editable = ("is_active", "order")  # 'order' is now allowed in list_editable
+    search_fields = ("name", "description")
+    list_filter = ("is_active",)
+    ordering = ("order",)
+
+@admin.register(ModerationAction)
+class ModerationActionAdmin(admin.ModelAdmin):
+    list_display = (
+        "performed_at", "moderator", "owner", "post", "media_item", 
+        "old_status", "new_status", "rejection_reason"
+    )
+    list_filter = ("performed_at", "new_status", "rejection_reason")
+    search_fields = ("moderator__username", "owner__username", "comment")
+    readonly_fields = ("performed_at",)
+    autocomplete_fields = ("post", "media_item", "moderator", "owner", "rejection_reason")
