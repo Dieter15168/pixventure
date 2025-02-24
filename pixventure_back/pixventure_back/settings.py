b28cc8a8e2ac3d95b8c6216e030e71ac98a25593
Daniel <daniil.gavr@gmail.com>
2025-02-24 21:46:21 +0300

    Set up CORS and next proxy for proper media load in dev environment

--- pixventure_back/pixventure_back/settings.py
+++ pixventure_back/pixventure_back/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'accounts',
     'albums',
     'analytics',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media_files')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- pixventure_front/next.config.ts
+++ pixventure_front/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    // Only enable proxy in development
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/media/:path*',
+          destination: 'http://127.0.0.1:8000/media/:path*', // Dev backend server
+        },
+      ];
+    }
+
+    // Production: no rewrites
+    return [];
+  },
 };
 
 export default nextConfig;
