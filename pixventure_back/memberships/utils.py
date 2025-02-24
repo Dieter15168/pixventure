806f9e456039fcd3ec3df591a6e894015d1f65d1
Daniel <daniil.gavr@gmail.com>
2025-02-24 03:02:45 +0300

    Fix check_if_user_is_paying helper function to handle unauthenticated user

--- pixventure_back/memberships/utils.py
+++ pixventure_back/memberships/utils.py
@@ -1,3 +1,6 @@
 def check_if_user_is_paying(user):
     # Query user memberships or do a cached property
-    return user.memberships.filter(is_active=True).exists()
+    if user.is_authenticated:
+        return user.memberships.filter(is_active=True).exists()
+    else:
+        return False
