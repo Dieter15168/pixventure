a6edb383c03d02b46860c878aa7fdef5d833eb76
Daniel <daniil.gavr@gmail.com>
2025-03-25 01:33:54 +0300

    Add admin class for gourl callback log

--- pixventure_back/integrations/gourl/admin.py
+++ pixventure_back/integrations/gourl/admin.py
@@ -1,7 +1,7 @@
 # integrations/gourl/admin.py
 
 from django.contrib import admin
-from .models import GoURLConfig
+from .models import GoURLConfig, PaymentCallbackLog
 
 
 @admin.register(GoURLConfig)
@@ -14,3 +14,12 @@ class GoURLConfigAdmin(admin.ModelAdmin):
     def private_key_short(self, obj):
         return f"{obj.private_key[:10]}..." if len(obj.private_key) > 10 else obj.private_key
     private_key_short.short_description = 'Private Key'
+
+
+@admin.register(PaymentCallbackLog)
+class PaymentCallbackLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'transaction', 'user', 'status', 'confirmed', 'created_at')
+    list_filter = ('status', 'confirmed', 'created_at')
+    search_fields = ('transaction__id', 'user__username', 'user__email')
+    ordering = ('-created_at',)
+    readonly_fields = ('raw_data', 'created_at')
